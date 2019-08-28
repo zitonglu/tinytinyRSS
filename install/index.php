ec9ec30e8461703774807e7f99f28bbd660a5ab3
xie <910109610@qq.com>
2019-08-28 10:34:50 +0800

    install

--- install/index.php
+++ install/index.php
@@ -25,7 +25,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tiny Tiny RSS - Installer</title>
+	<title>feed.KIM - 安装程序</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<style type="text/css">
 		textarea { font-size : 12px; }
@@ -85,39 +85,39 @@
 		$errors = array();
 
 		if (version_compare(PHP_VERSION, '5.6.0', '<')) {
-			array_push($errors, "PHP version 5.6.0 or newer required. You're using " . PHP_VERSION . ".");
+			array_push($errors, "需要5.6.0或更高版本的PHP，您用的版本是 " . PHP_VERSION . ".");
 		}
 
 		if (!function_exists("curl_init") && !ini_get("allow_url_fopen")) {
-			array_push($errors, "PHP configuration option allow_url_fopen is disabled, and CURL functions are not present. Either enable allow_url_fopen or install PHP extension for CURL.");
+			array_push($errors, "php配置选项allow_url_fopen被禁用，curl函数不存在。启用allow_url_fopen或安装curl的php扩展。");
 		}
 
 		if (!function_exists("json_encode")) {
-			array_push($errors, "PHP support for JSON is required, but was not found.");
+			array_push($errors, "需要对JSON的PHP支持，但未找到。");
 		}
 
 		if (!class_exists("PDO")) {
-			array_push($errors, "PHP support for PDO is required but was not found.");
+			array_push($errors, "需要PHP支持PDO，但未找到。");
 		}
 
 		if (!function_exists("mb_strlen")) {
-			array_push($errors, "PHP support for mbstring functions is required but was not found.");
+			array_push($errors, "需要对mbstring函数的PHP支持，但未找到。");
 		}
 
 		if (!function_exists("hash")) {
-			array_push($errors, "PHP support for hash() function is required but was not found.");
+			array_push($errors, "需要对hash()函数的php支持，但未找到。");
 		}
 
 		if (!function_exists("iconv")) {
-			array_push($errors, "PHP support for iconv is required to handle multiple charsets.");
+			array_push($errors, "处理多个字符集需要对iconv的PHP支持。");
 		}
 
 		if (ini_get("safe_mode")) {
-			array_push($errors, "PHP safe mode setting is obsolete and not supported by tt-rss.");
+			array_push($errors, "PHP安全模式设置已过时，feedKIM不支持。");
 		}
 
 		if (!class_exists("DOMDocument")) {
-			array_push($errors, "PHP support for DOMDocument is required, but was not found.");
+			array_push($errors, "需要对domdocument的PHP支持，但未找到。");
 		}
 
 		return $errors;
@@ -196,7 +196,7 @@
 
 ?>
 
-<h1>Tiny Tiny RSS Installer</h1>
+<h1>feed.Keep In Myself 安装程序</h1>
 
 <div class='content'>
 
@@ -206,10 +206,10 @@
 		require "../config.php";
 
 		if (!defined('_INSTALLER_IGNORE_CONFIG_CHECK')) {
-			print_error("Error: config.php already exists in tt-rss directory; aborting.");
+			print_error("Error: config.php 已经存在，请删除后再安装程序");
 
 			print "<form method='GET' action='../index.php'>
-				<button type='submit' dojoType='dijit.form.Button' class='alt-primary'>Return to Tiny Tiny RSS</button>
+				<button type='submit' dojoType='dijit.form.Button' class='alt-primary'>返回feed.KIM</button>
 				</form>";
 			exit;
 		}
@@ -233,7 +233,7 @@
 <form action="" method="post">
 	<input type="hidden" name="op" value="testconfig">
 
-	<h2>Database settings</h2>
+	<h2>数据库参数设置</h2>
 
 	<?php
 		$issel_pgsql = $DB_TYPE == "pgsql" ? "selected='selected'" : "";
@@ -241,61 +241,61 @@
 	?>
 
 	<fieldset>
-		<label>Database type:</label>
+		<label>数据库类型:</label>
 		<select name="DB_TYPE" dojoType="dijit.form.Select">
-			<option <?php echo $issel_pgsql ?> value="pgsql">PostgreSQL</option>
 			<option <?php echo $issel_mysql ?> value="mysql">MySQL</option>
+			<option <?php echo $issel_pgsql ?> value="pgsql">PostgreSQL</option>
 		</select>
 	</fieldset>
 
 	<fieldset>
-		<label>Username:</label>
+		<label>登录名字:</label>
 		<input dojoType="dijit.form.TextBox" required name="DB_USER" size="20" value="<?php echo $DB_USER ?>"/>
 	</fieldset>
 
 	<fieldset>
-		<label>Password:</label>
+		<label>登录密码:</label>
 		<input dojoType="dijit.form.TextBox" name="DB_PASS" size="20" type="password" value="<?php echo $DB_PASS ?>"/>
 	</fieldset>
 
 	<fieldset>
-		<label>Database name:</label>
+		<label>数据库名称:</label>
 		<input dojoType="dijit.form.TextBox" required name="DB_NAME" size="20" value="<?php echo $DB_NAME ?>"/>
 	</fieldset>
 
 	<fieldset>
-		<label>Host name:</label>
+		<label>主机名:</label>
 		<input dojoType="dijit.form.TextBox" name="DB_HOST" size="20" value="<?php echo $DB_HOST ?>"/>
-		<span class="hint">If needed</span>
+		<span class="hint">如果需要可以设置</span>
 	</fieldset>
 
 	<fieldset>
-		<label>Port:</label>
+		<label>端口:</label>
 		<input dojoType="dijit.form.TextBox" name="DB_PORT" type="number" size="20" value="<?php echo $DB_PORT ?>"/>
 		<span class="hint">Usually 3306 for MySQL or 5432 for PostgreSQL</span>
 	</fieldset>
 
-	<h2>Other settings</h2>
+	<h2>其他设置</h2>
 
-	<p>This should be set to the location your Tiny Tiny RSS will be available on.</p>
+	<p>设置软件具体所在路径</p>
 
 	<fieldset>
-		<label>Tiny Tiny RSS URL:</label>
+		<label>feed.KIM URL:</label>
 		<input dojoType="dijit.form.TextBox" type="url" name="SELF_URL_PATH" placeholder="<?php echo $SELF_URL_PATH; ?>" value="<?php echo $SELF_URL_PATH ?>"/>
 	</fieldset>
 
-	<p><button type="submit" dojoType="dijit.form.Button" class="alt-primary">Test configuration</button></p>
+	<p><button type="submit" dojoType="dijit.form.Button" class="alt-primary">测试配置</button></p>
 </form>
 
 <?php if ($op == 'testconfig') { ?>
 
-	<h2>Checking configuration</h2>
+	<h2>正在检查配置</h2>
 
 	<?php
 		$errors = sanity_check($DB_TYPE);
 
 		if (count($errors) > 0) {
-			print "<p>Some configuration tests failed. Please correct them before continuing.</p>";
+			print "<p>某些配置测试失败，请在继续之前更正它们。</p>";
 
 			print "<ul>";
 
@@ -311,15 +311,15 @@
 		$notices = array();
 
 		if (!function_exists("curl_init")) {
-			array_push($notices, "It is highly recommended to enable support for CURL in PHP.");
+			array_push($notices, "强烈建议在PHP中启用对curl的支持。");
 		}
 
 		if (function_exists("curl_init") && ini_get("open_basedir")) {
-			array_push($notices, "CURL and open_basedir combination breaks support for HTTP redirects. See the FAQ for more information.");
+			array_push($notices, "url和open-basedir组合中断了对HTTP重定向的支持。有关更多信息，请参阅常见问题解答。");
 		}
 
 		if (!function_exists("idn_to_ascii")) {
-			array_push($notices, "PHP support for Internationalization Functions is required to handle Internationalized Domain Names.");
+			array_push($notices, "处理国际化域名需要对国际化函数的PHP支持。");
 		}
 
         if ($DB_TYPE == "mysql" && !function_exists("mysqli_connect")) {
@@ -327,11 +327,11 @@
         }
 
         if ($DB_TYPE == "pgsql" && !function_exists("pg_connect")) {
-			array_push($notices, "PHP extension for PostgreSQL is missing. This may prevent legacy plugins from working.");
+			array_push($notices, "缺少mysql（mysqli）的php扩展，这可能会阻止旧插件工作。 ");
         }
 
 		if (count($notices) > 0) {
-			print_notice("Configuration check succeeded with minor problems:");
+			print_notice("配置检查成功，但有一些小问题：");
 
 			print "<ul>";
 
@@ -341,33 +341,33 @@
 
 			print "</ul>";
 		} else {
-			print_notice("Configuration check succeeded.");
+			print_notice("配置检查成功");
 		}
 
 	?>
 
-	<h2>Checking database</h2>
+	<h2>正在检查数据库</h2>
 
 	<?php
 		$pdo = pdo_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_TYPE, $DB_PORT);
 
 		if (!$pdo) {
-			print_error("Unable to connect to database using specified parameters (driver: $DB_TYPE).");
+			print_error("无法使用指定的参数（driver: $DB_TYPE）连接到数据库。");
 			exit;
 		}
 
-		print_notice("Database test succeeded.");
+		print_notice("数据库测试成功");
 	?>
 
-	<h2>Initialize database</h2>
+	<h2>初始化数据库</h2>
 
-	<p>Before you can start using tt-rss, database needs to be initialized. Click on the button below to do that now.</p>
+	<p>在开始使用feed.KIM之前，需要初始化数据库。现在单击下面的按钮。</p>
 
 	<?php
 		$res = $pdo->query("SELECT true FROM ttrss_feeds");
 
 		if ($res && $res->fetch()) {
-			print_error("Some tt-rss data already exists in this database. If you continue with database initialization your current data <b>WILL BE LOST</b>.");
+			print_error("此数据库中已存在某些feed.KIM数据。如果继续数据库初始化，则当前数据<b>将会丢失</b>.");
 			$need_confirm = true;
 		} else {
 			$need_confirm = false;
@@ -389,9 +389,9 @@
 		<p>
 		<?php if ($need_confirm) { ?>
 			<button onclick="return confirm('Please read the warning above. Continue?')" type="submit"
-					   class="alt-danger" dojoType="dijit.form.Button">Initialize database</button>
+					   class="alt-danger" dojoType="dijit.form.Button">初始化数据库</button>
 		<?php } else { ?>
-			<button type="submit" class="alt-danger" dojoType="dijit.form.Button">Initialize database</button>
+			<button type="submit" class="alt-danger" dojoType="dijit.form.Button">初始化数据库</button>
 		<?php } ?>
 		</p>
 	</form>
@@ -408,7 +408,7 @@
 
 		<input type="hidden" name="op" value="skipschema">
 
-		<p><button type="submit" dojoType="dijit.form.Button">Skip initialization</button></p>
+		<p><button type="submit" dojoType="dijit.form.Button">跳过初始化</button></p>
 	</form>
 
 	</td></tr></table>
@@ -426,7 +426,7 @@
 
 			if ($op == 'installschema') {
 
-				print "<h2>Initializing database...</h2>";
+				print "<h2>正在初始化数据库… </h2>";
 
 				$lines = explode(";", preg_replace("/[\r\n]/", "",
                     file_get_contents("../schema/ttrss_schema_".basename($DB_TYPE).".sql")));
@@ -442,17 +442,17 @@
 					}
 				}
 
-				print_notice("Database initialization completed.");
+				print_notice("数据库初始化完成");
 
 			} else {
-				print_notice("Database initialization skipped.");
+				print_notice("已跳过数据库初始化");
 			}
 
-			print "<h2>Generated configuration file</h2>";
+			print "<h2>生成的配置文件</h2>";
 
-			print "<p>Copy following text and save as <code>config.php</code> in tt-rss main directory. It is suggested to read through the file to the end in case you need any options changed fom default values.</p>";
+			print "<p>复制以下文本，并另存为主目录中的<code>config.php<code>如果需要更改任何选项的默认值，建议将文件读懂。</p>";
 
-			print "<p>After copying the file, you will be able to login with default username and password combination: <code>admin</code> and <code>password</code>. Don't forget to change the password immediately!</p>"; ?>
+			print "<p>复制文件后，您将能够使用默认用户名和密码组合登录：<code>admin<code>和<code>password<code>。别忘了马上更改密码！ </p>"; ?>
 
 			<form action="" method="post">
 				<input type="hidden" name="op" value="saveconfig">
@@ -471,19 +471,19 @@
 			<hr/>
 
 			<?php if (is_writable("..")) { ?>
-				<p>We can also try saving the file automatically now.</p>
+				<p>我们现在也可以尝试自动保存文件</p>
 
-				<p><button type="submit" dojoType='dijit.form.Button' class='alt-primary'>Save configuration</button></p>
+				<p><button type="submit" dojoType='dijit.form.Button' class='alt-primary'>保存配置</button></p>
 				</form>
 			<?php } else {
-				print_error("Unfortunately, parent directory is not writable, so we're unable to save config.php automatically.");
+				print_error("很遗憾，父目录不可写，因此我们无法自动保存config.php");
 			}
 
-		   print_notice("You can generate the file again by changing the form above.");
+		   print_notice("您可以通过更改上面的表单再次生成文件");
 
 		} else if ($op == "saveconfig") {
 
-			print "<h2>Saving configuration file to parent directory...</h2>";
+			print "<h2>正在将配置文件保存到父目录… </h2>";
 
 			if (!file_exists("../config.php")) {
 
@@ -495,18 +495,18 @@
 						$DB_PORT, $SELF_URL_PATH));
 
 					if ($written > 0) {
-						print_notice("Successfully saved config.php. You can try <a href=\"..\">loading tt-rss now</a>.");
+						print_notice("成功保存config.php,您可以尝试现在加载feed.KIM</a>.");
 
 					} else {
-						print_notice("Unable to write into config.php in tt-rss directory.");
+						print_notice("无法写入目录中的config.php");
 					}
 
 					fclose($fp);
 				} else {
-					print_error("Unable to open config.php in tt-rss directory for writing.");
+					print_error("无法在目录中打开config.php进行写入");
 				}
 			} else {
-				print_error("config.php already present in tt-rss directory, refusing to overwrite.");
+				print_error("config.php已经存在于目录中，拒绝覆盖");
 			}
 		}
 	?>
